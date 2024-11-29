3c6e7a05329fc3d3ba83469ee1b0513d0c2aa074
JulienGha <julien.ghali@efrei.net>
2024-11-29 13:34:04 +0100

    clusters

--- server_pdf.py
+++ server_pdf.py
@@ -91,7 +91,8 @@ def perform_clustering():
     global df_emails, cluster_names, fig_json, X_embedded
 
     # Specify the folder path
-    folder_path = r'/home/administrator/mail_infra'  # Update this path to your folder
+    folder_path = r'/home/administrator/mail_infra'
+    # folder_path = r'C:\Users\JGH\Documents\Mail semaine du 4 au 8 nov'
 
     # Initialize lists to store email contents and metadata
     emails = []
@@ -174,7 +175,7 @@ def perform_clustering():
 
     # Determine the optimal number of clusters using the Elbow Method
     # You can adjust this range as needed
-    cluster_range = range(5, 21)  # Trying 5 to 20 clusters
+    cluster_range = range(5, 80)  # Trying 5 to 20 clusters
     inertia = []
     silhouette_scores = []
 
